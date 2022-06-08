721a900d37209f0c4697c9adacc5fd41f810495d
Florian Taute <tautellini@outlook.de>
2022-06-08 13:22:50 +0200

    add navigation route

--- app/src/main/java/com/example/starwarsplanets/navigation/NavGraph.kt
+++ app/src/main/java/com/example/starwarsplanets/navigation/NavGraph.kt
@@ -2,9 +2,12 @@ package com.example.starwarsplanets.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.starwarsplanets.features.planets.PlanetsScreen
+import androidx.navigation.navArgument
+import com.example.starwarsplanets.features.planet.presentation.PlanetScreen
+import com.example.starwarsplanets.features.planets.presentation.PlanetsScreen
 
 @Composable
 fun NavGraph(navController: NavHostController) {
@@ -14,6 +17,15 @@ fun NavGraph(navController: NavHostController) {
     ) {
         composable(route = Route.Planets.route) {
             PlanetsScreen(navController = navController)
+        }
+        composable(
+            route = Route.Planet.route,
+            arguments = listOf(navArgument("id") {
+                type = NavType.LongType
+            })
+        ) { backStackEntry ->
+            backStackEntry.arguments?.getLong("id")
+                ?.let { PlanetScreen(it, navController = navController) }
         }
     }
 }
--- app/src/main/java/com/example/starwarsplanets/navigation/Route.kt
+++ app/src/main/java/com/example/starwarsplanets/navigation/Route.kt
@@ -2,4 +2,7 @@ package com.example.starwarsplanets.navigation
 
 sealed class Route(val route: String) {
     object Planets : Route("planets_screen")
+    object Planet : Route("planet_screen/{id}") {
+        fun passId(id: Long) = "planet_screen/$id"
+    }
 }
